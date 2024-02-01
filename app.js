acb5db1a9dd9c429ea84b53de128574205dfa2d6
StudentUserDiego03 <dcardenascastane@student.alamo.edu>
2024-02-01 16:40:57 -0600

    fixed other function

--- app.js
+++ app.js
@@ -7,10 +7,12 @@ function myFunction(){
     document.getElementById("electronicDevice").innerHTML = 
     "Name of Product: " + device1.productName() + "<br>" + "initial price: $" + device1.price
     + "<br>" + "tax: $" + tax + "<br>" + "final price: $" + finalPrice;
-}
+};
 
-function newFunction(){
-    const device2 = {type2: "computer", brand2: "microsoft", price2: 1200, 
+function otherFunction(){
+    const decvice2 = {type2: "laptop", brand2: "Microsoft", price2: 900, 
     productName2: function(){return this.brand2 + " " + this.type2;}};
-    document.getElementById("ElectronicDevice2").innerHTML = "Name of Product: " + device2.productName2 ;
+    let finalPrice = decvice2.price2 + tax;
+    document.getElementById("electronicDevice2").innerHTML= "Name of Product: " + decvice2.productName2() + "<br>"
+    + "initial price: $" + decvice2.price2 + "<br>" + "tax: $" + tax + "<br>" + "final price: $" + finalPrice;
 }
